d758f8ba7f7cfb6c069d44dda8ae7e75a176725f
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2023-10-08 19:06:55 +0300

    updated preprocessing

--- 2-preprocess.py
+++ 2-preprocess.py
@@ -1,7 +1,9 @@
 """This module is responsible for preprocessing the data in the `data/raw` folder and storing it in the `data/processed` folder."""
+import pandas as pd
 from src.preprocessing import preprocess_fns
 
 if __name__ == "__main__":
+    res = pd.DataFrame(columns=["text", "author"])
     for fn in preprocess_fns:
-        df = fn()
-    df.to_csv(f"data/processed/data.csv", index=False)
+        res = pd.concat([res, fn()])
+    res.to_csv(f"data/processed/data.csv", index=False)
